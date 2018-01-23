b024807952772aa643bb71c6cad98edd0a3314c0
Datream Tanti <datreamsailo@gmail.com>
2018-01-23 22:11:25 +0530

    Update Area_Calculator.py

--- Semister 1/Area_Calculator.py
+++ Semister 1/Area_Calculator.py
@@ -1,38 +1,38 @@
 import math
 
 
-# To calculate area
+# To calculate area of circles, rectangles and triangles
+
+def input_check(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+            continue
+        if value < 0:
+            print("Sorry, your response must not be negative.")
+            continue
+        else:
+            break
+    return value
+
 
 def area_circle():
-    radius = input("Enter the value of radius: ")
-    if radius.isalpha():
-        # For string Input
-        print("Error, wrong input")
-        area_circle()
-    else:
-        print("The area is", format(math.pi * (float(radius) ** 2), '.2f'))
+    radius = input_check("Enter the value of radius: ")
+    print("The area is", format((math.pi * radius ** 2), '.2f'))
 
 
 def area_triangle():
-    base = input("Enter the value of base: ")
-    height = input("Enter the value of height: ")
-    if base.isalpha() == True or height.isalpha() == True:
-        # For string Input
-        print("Error, wrong input")
-        area_triangle()
-    else:
-        print("The area is", format(0.5 * abs(float(base)) * abs(float(height)), '.2f'))
+    base = input_check("Enter the value of base: ")
+    height = input_check("Enter the value of height: ")
+    print("The area is", (format(0.5 * base * height), '.2f'))
 
 
 def area_rectangle():
-    base = input("Enter the value of base: ")
-    height = input("Enter the value of height: ")
-    if base.isalpha() == True or height.isalpha() == True:
-        # For string Input
-        print("Error, wrong input")
-        area_rectangle()
-    else:
-        print("The area is", format(abs(float(base)) * abs(float(height)), '.2f'))
+    base = input_check("Enter the value of base: ")
+    height = input_check("Enter the value of height: ")
+    print("The area is", (format(base * height), '.2f'))
 
 
 def switch(case):
@@ -42,8 +42,9 @@ def switch(case):
         area_triangle()
     elif case == 'rectangle':
         area_rectangle()
-    else:
-        switch((input("Error, re-enter input: ")).lower())
 
 
-switch((input("Would you like to find the area of a circle, a triangle or a rectangle? ")).lower())
+data = (input("Would you like to find the area of a circle, a triangle or a rectangle? ")).lower()
+while data not in ('circle', 'triangle', 'rectangle'):
+    data = (input("Error, re-enter input: ")).lower()
+switch(data)
